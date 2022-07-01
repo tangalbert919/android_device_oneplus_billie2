7b3cd5328be91db8de1b519b434bd21e97d5be09
Albert Tang <tangalbert919@yahoo.com>
2022-07-01 18:29:58 -0500

    billie2: Regenerate proprietary blobs list
    Change-Id: I768f5114c21ab1613a0cd681427107d8f2fcf368

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,7 @@ function blob_fixup() {
             sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "1F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "27 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
