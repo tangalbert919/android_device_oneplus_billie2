cedb3e165b16e0d9e0764ebd33af2fa6e4f8eda2
Albert Tang <tangalbert919@yahoo.com>
2022-08-26 21:59:21 -0500

    billie2: Update from OOS 11.0.7.BE81AA
    Change-Id: Ieff0af2c70e304e09ed167f6c2c3b8f16c689361

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,7 @@ CUSTOM_APNS_FILE := $(DEVICE_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-04-01
+VENDOR_SECURITY_PATCH := 2022-06-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
