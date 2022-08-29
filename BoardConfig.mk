95a08d290c8861be11b4e3d3b52ba6b0cef8fa06
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    billie2: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,7 +194,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-04-05
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
