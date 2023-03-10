c33a97142cadd8595391e72db0f328ae908530f9
Albert Tang <tangalbert919@yahoo.com>
2023-03-10 10:07:11 -0600

    billie2: Import QCOM USB Soong namespace
    Change-Id: Ib861320c711a76a495e1ff8f7c445077e09a20cc

--- device.mk
+++ device.mk
@@ -477,6 +477,9 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
