4d0ebfa8af918834d1ed2a9e0711efb0d0ce9580
Edwin Moquete <edwinmmoquete@gmail.com>
2023-08-01 01:34:32 +0000

    billie2: Remove vendor.oneplus.hardware.display@1.0 service
    No longer needed for UDFPS, which we don't have anyway
    
    Change-Id: Ifb896fb286c8d03eb8e88f0426cd972f990b0a32

--- device.mk
+++ device.mk
@@ -258,7 +258,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
-    vendor.oneplus.hardware.display@1.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.0.vendor \
