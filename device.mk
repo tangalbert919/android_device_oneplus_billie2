1daa29865a9c00f06604f89a0837e0ae37a7c321
Albert Tang <tangalbert919@yahoo.com>
2024-06-18 10:22:50 -0500

    billie2: Cleanup and update vendor.display.config
    Change-Id: Ibe2dbd8d93f7551b65cbac7eefeb765b5a8e9d09

--- device.mk
+++ device.mk
@@ -246,17 +246,8 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.bengal \
-    vendor.display.config@1.0 \
     vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor \
-    vendor.display.config@1.4.vendor \
-    vendor.display.config@1.5.vendor \
-    vendor.display.config@1.6.vendor \
-    vendor.display.config@1.7.vendor \
-    vendor.display.config@1.8.vendor \
-    vendor.display.config@1.9.vendor \
+    vendor.display.config@1.15.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
